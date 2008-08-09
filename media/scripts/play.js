eed0aec0d2909d69b7339a3df48cb1c627e4c031
Peter Stradinger <peter@cutopia.(none)>
2008-08-09 15:39:35 -0700

    playback and clicks are working right, but clicks after the first one don't display properly.

--- media/scripts/play.js
+++ media/scripts/play.js
@@ -6,7 +6,8 @@ emlprime.play = function () {
 	playback_position: 0,
 	playback_limit: 1,
         click_delay_limit: 5000,  // 5 second pause between clicks
-        playback_delay: 500,     // 1 second pause between lighting up on playback
+        playback_delay: 500,     // half second pause between lighting up on playback
+	switch_playback_mode_delay: 2000, // 2 second pause between the last click and playing back the next set
         set_on: function(color, callback) {
             // light up a light of the appropriate color
 	    var button = $("#game #" + color + " img"),
@@ -24,28 +25,30 @@ emlprime.play = function () {
 	    var button = $("#game #" + color + " img"),
 	    src_off = button.attr("src").replace("_light","");
 	    button.attr("src", src_off);
-	    if (self.playback_position < self.playback_limit) {
-		self.playback_position += 1;
-		}
 	    if (typeof callback != 'undefined') {
 		setTimeout(function () { callback(); }, self.playback_delay);
 	    }
 	},
 	click_handler: function (event) {
 	    var color = $(event.target).attr('alt');
-	    console.log(self.clicked_sequence.length >= self.playback_limit);
-	    if (self.clicked_sequence.length >= self.playback_limit) {
+	    self.clicked_sequence.push(color);
+	    var playback_length = (self.playback_limit - 1), // We've already incremented playback limit
+	        clicked_sequence_length = self.clicked_sequence.length;
+	    
+	    console.log("clicked sequence length:" + clicked_sequence_length);
+	    console.log("playback length:" + playback_length);
+	    console.log(clicked_sequence_length >= playback_length);
+
+	    if (clicked_sequence_length >= playback_length) {
 		callback = self.playback;
-		self.playback_limit += 1;
-		self.playback_position = 0;
 	    } else {
 		callback = undefined;
+		
 	    }
-	    self.clicked_sequence.push(color);
 	    
 	    self.set_on(color, callback);
+
 	    $('#game img').unbind("click");
-	    self.playback_limit += 1;
 	    console.log(self.clicked_sequence);
 	},
 	load_answer_key: function(data) {
@@ -57,8 +60,14 @@ emlprime.play = function () {
 	playback: function() {
 	    console.log("position:"+self.playback_position);
 	    console.log("limit:"+self.playback_limit);
+	    var delay = (self.playback_position == 0) ? self.switch_playback_mode_delay : 0;
 	    if (self.playback_position < self.playback_limit) {
-		self.set_on(self.key_sequence[self.playback_position], self.playback);
+		setTimeout(function () {
+			self.set_on(self.key_sequence[self.playback_position], self.playback);
+			self.playback_position += 1;
+		    },
+		    delay
+		    );
 	    } else {
 		self.playback_position = 0;
 		self.playback_limit += 1;
